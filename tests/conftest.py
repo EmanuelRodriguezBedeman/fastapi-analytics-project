c53cb53f2b342dd8e3e91579fd053a701afb6a3f
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2026-02-21 22:57:11 -0300

    Feat: Add test conf to ignore rate limiter

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from app.main import app
+from app.utils.rate_limiter import rate_limit_dependency
+
+
+@pytest.fixture(autouse=True)
+def disable_rate_limiter():
+    """
+    Globally disables the rate limiter for all tests to avoid HTTP 429 errors.
+    """
+    app.dependency_overrides[rate_limit_dependency] = lambda: None
+    yield
+    app.dependency_overrides.clear()
